0ad7a3b8b4d1de6c7e29c91c38e64c933d184800
Joel Shapiro <joel@presence-labs.com>
2017-11-14 16:40:22 -0800

    Completed Home Screen: Container.

---
+++ app/components/Container/Container.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+import { View } from 'react-native';
+import styles from './styles';
+
+const Container = ({ children }) => <View style={styles.container}>{children}</View>;
+
+Container.PropTypes = {
+  children: PropTypes.number.isRequired,
+};
+
+export default Container;
