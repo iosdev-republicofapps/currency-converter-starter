586adb27b55beb81b9f1326e66ccc9c895508f7d
Joel Shapiro <joel@presence-labs.com>
2017-11-18 16:47:17 -0800

    TEMP COMMIT.

---
+++ app/components/TextInput/InputWithButton.js
@@ -0,0 +1,7 @@
+import React, { PropTypes } from 'react';
+import { View, Text, TouchableHighlight, TextInput } from 'react-native';
+import styles from './styles';
+
+const InputWithButton = () => null;
+
+export default InputWithButton;
