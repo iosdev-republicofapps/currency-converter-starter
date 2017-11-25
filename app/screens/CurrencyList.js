474d89ca8d7df0944fbe5e281fe464aee5738362
iosdev-republicofapps <iosdev@republicofapps.com>
2017-11-24 19:35:26 -0800

    Completed: Currency List Screen: FlatList.

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Text, FlatList, ScrollView } from 'react-native';
+import { Text, FlatList, ScrollView, View, StatusBar } from 'react-native';
 
 import currencies from '../data/currencies';
 
 const CurrencyList = () => (
-  <FlatList data={currencies} renderItem={({ item }) => <Text>{item}</Text>} />
+  <View style={{ flex: 1 }}>
+    <StatusBar barStyle="default" translucent={false} />
+    <FlatList
+      data={currencies}
+      renderItem={({ item }) => <Text>{item}</Text>}
+      keyExtractor={item => item}
+    />
+  </View>
 );
 
 export default CurrencyList;
