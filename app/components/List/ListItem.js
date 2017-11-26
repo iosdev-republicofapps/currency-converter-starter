c39ead2521743760f96f04211c68289d8b1f165c
iosdev-republicofapps <iosdev@republicofapps.com>
2017-11-25 17:41:22 -0800

    Completed Themes Screen.

--- app/components/List/ListItem.js
+++ app/components/List/ListItem.js
@@ -10,11 +10,16 @@ const ListItem = ({
   checkmark = true,
   visible = true,
   customIcon = null,
+  iconBackground,
 }) => (
   <TouchableHighlight onPress={onPress} underlayColor={styles.$underlayColor}>
     <View style={styles.row}>
       <Text style={styles.text}>{text}</Text>
-      {selected ? <Icon checkmark={checkmark} visible={visible} /> : <Icon />}
+      {selected ? (
+        <Icon checkmark={checkmark} visible={visible} iconBackground={iconBackground} />
+      ) : (
+        <Icon />
+      )}
       {customIcon}
     </View>
   </TouchableHighlight>
@@ -27,6 +32,7 @@ ListItem.PropTypes = {
   checkmark: PropTypes.bool,
   visible: PropTypes.bool,
   customIcon: PropTypes.element,
+  iconBackground: PropTypes.string,
 };
 
 export default ListItem;
--- app/index.js
+++ app/index.js
@@ -3,9 +3,13 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 import Home from './screens/Home';
 import CurrencyList from './screens/CurrencyList';
 import Options from './screens/Options';
+import Themes from './screens/Themes';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
+  $primaryOrange: '#d57a66',
+  $primaryGreen: '#00bd9d',
+  $primaryPurple: '#9e768f',
   $white: '#fff',
   $border: '#e2e2e2',
   $inputText: '#797979',
@@ -15,4 +19,5 @@ EStyleSheet.build({
 
 // export default () => <Home />;
 // export default () => <CurrencyList />;
-export default () => <Options />;
+// export default () => <Options />;
+export default () => <Themes />;
