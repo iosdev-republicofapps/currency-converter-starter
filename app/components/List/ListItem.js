521eaf252a43cca0a1f1d79fa2a8bd665bbe249e
iosdev-republicofapps <iosdev@republicofapps.com>
2017-11-25 17:21:55 -0800

    Complete Options Screen.

--- app/components/List/ListItem.js
+++ app/components/List/ListItem.js
@@ -3,11 +3,19 @@ import { View, Text, TouchableHighlight } from 'react-native';
 import Icon from './Icon';
 import styles from './styles';
 
-const ListItem = ({ text, onPress, selected = false, checkmark = true, visible = true }) => (
+const ListItem = ({
+  text,
+  onPress,
+  selected = false,
+  checkmark = true,
+  visible = true,
+  customIcon = null,
+}) => (
   <TouchableHighlight onPress={onPress} underlayColor={styles.$underlayColor}>
     <View style={styles.row}>
       <Text style={styles.text}>{text}</Text>
       {selected ? <Icon checkmark={checkmark} visible={visible} /> : <Icon />}
+      {customIcon}
     </View>
   </TouchableHighlight>
 );
@@ -18,6 +26,7 @@ ListItem.PropTypes = {
   selected: PropTypes.bool,
   checkmark: PropTypes.bool,
   visible: PropTypes.bool,
+  customIcon: PropTypes.element,
 };
 
 export default ListItem;
---
+++ app/screens/Options.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { ScrollView, StatusBar, View, Platform } from 'react-native';
+import { ListItem, Separator } from '../components/List';
+import { Ionicons } from '@expo/vector-icons';
+
+const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
+const ICON_COLOR = '#868686';
+const ICON_SIZE = 23;
+
+class Options extends Component {
+  handleThemesPress = () => {
+    console.log('press themes');
+  };
+
+  handleSitePress = () => {
+    console.log('press site');
+  };
+
+  render() {
+    return (
+      <ScrollView>
+        <StatusBar translucent={false} barStyle="default" />
+        {/* To avoid notch. */}
+        <View style={{ height: 44 }} />
+        <ListItem
+          text="Themes"
+          onPress={this.handleThemesPress}
+          customIcon={
+            <Ionicons name={`${ICON_PREFIX}-arrow-forward`} color={ICON_COLOR} size={ICON_SIZE} />
+          }
+        />
+        <Separator />
+        <ListItem
+          text="Fixer.io"
+          onPress={this.handleSitePress}
+          customIcon={<Ionicons name={`${ICON_PREFIX}-link`} color={ICON_COLOR} size={ICON_SIZE} />}
+        />
+      </ScrollView>
+    );
+  }
+}
+
+export default Options;
